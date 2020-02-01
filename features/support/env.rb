fb80b4ea76a4900fa9313e6cef1d5565b0f183cf
christ47 <cashifechristie@gmail.com>
2020-02-01 18:22:30 +0000

    Repo structured

---
+++ features/support/env.rb
@@ -0,0 +1,26 @@
+require 'capybara/cucumber'
+require 'rspec'
+require 'selenium-webdriver'
+
+require_relative '../lib/C_A_Module'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+  end
+
+Capybara.register_driver(:chrome) do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.configure do |config|
+    config.ignore_hidden_elements = false
+    config.default_max_wait_time = 15
+    config.default_driver = :chrome
+end
+
+at_exit do
+    time = Time.now.strftime('%Y/%m/%d %H:%M:%S')
+    puts "End of test conducted at #{time}"
+end
+
+World(C_A_Module)
